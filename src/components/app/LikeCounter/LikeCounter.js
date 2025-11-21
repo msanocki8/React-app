f8fc2f94c29de5c7b5cc7a547adffbfc96f10ac4
msanocki8 <maciejsanocki8@gmail.com>
2025-11-21 17:58:45 +0100

    first commit

---
+++ src/components/app/LikeCounter/LikeCounter.js
@@ -0,0 +1,11 @@
+import './LikeCounter.css';
+function LikeCounter(){
+    return (
+        <div className='post'>
+            <p>fajny post!!! liczba lików x</p>
+            <button>polub❤️</button>
+        </div>
+    );
+}
+
+export default LikeCounter;
---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './components/app/App';
+import LikeCounter from './components/app/LikeCounter/LikeCounter';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+    <div className='posts'>
+    <LikeCounter></LikeCounter>
+    <LikeCounter></LikeCounter>
+    <LikeCounter></LikeCounter> </div>
+  </React.StrictMode>
+ 
+);
+
+
